a52b135f8a9734ee825a47953d6d32a6e6ef9d5d
Madi Rei <madison.nicole.rei@gmail.com>
2025-09-22 17:42:55 -0700

    adds create_movie and add_to_watched functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,23 @@
 # ------------- WAVE 1 --------------------
 
+# If title, genre, and rating are all truthy, return a dictionary with the
+# movie data inside. If any of the given arguments are falsy, return None.
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        new_movie = {
+            "title": title, 
+            "genre": genre, 
+            "rating": rating
+            }
+        return new_movie
+    else:
+        return None
+
+# Adds movie to user_data. Each movie is a dictionary, formatted like new_movie.
+# user_data is a dictionary with a "watched" key and a value that's a list of
+# dictionaries. 
+def add_to_watched(user_data, movie):
+    return user_data["watched"].append(movie)
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
